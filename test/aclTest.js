85ec7145c66d7670738a9faa5cf1e16f478655f5
salboaie <master1!23>
2015-02-26 17:14:52 +0200

    working and tested version

--- test/aclTest.js
+++ test/aclTest.js
@@ -3,14 +3,21 @@ var acl = require("../lib/acl.js");
 var assert  = require('assert');
 
 var persistence =  acl.createMemoryPersistence();
-var writeConcern = acl.createConcern("write", persistence);
+var writeConcern = acl.createConcern("write", persistence, function(zoneId, resourceId, callback){
+    if(zoneId == "root"){
+        callback(null, true);
+    } else {
+        callback(null, false);
+    }
+});
 
 var counter = 0 ;
 
 persistence.addZoneParent("user_1", "role_1");
 
 persistence.loadZoneParents("user_1", function(err, res){
-    assert.equal(res[0], "role_1");
+    assert.equal(res[0], "user_1");
+    assert.equal(res[1], "role_1");
     counter++;
 });
 
@@ -19,8 +26,9 @@ persistence.addZoneParent("user_2", "role_2");
 persistence.addZoneParent("role_1", "admin");
 
 persistence.loadZoneParents("user_1", function(err, res){
-    assert.equal(res[0], "role_1");
-    assert.equal(res[1], "admin");
+    assert.equal(res[0], "user_1");
+    assert.equal(res[1], "role_1");
+    assert.equal(res[2], "admin");
     counter++;
 });
 
@@ -41,24 +49,50 @@ writeConcern.allow("user_1", "r_1", function(err, res){
 writeConcern.allow("user_2", "r_2", function(err, res){
     assert.equal(res, false);
     counter++;
+
+    persistence.addResourceParent("m_2", "g_x");
+    writeConcern.grant("user", "g_x");
+
+    writeConcern.allow("user_2", "r_2", function(err, res){
+        assert.equal(res, true);
+        counter++;
+    });
 });
 
-persistence.addResourceParent("f_1", "g_1");
-writeConcern.grant("user", "g_1");
-
-writeConcern.allow("user_2", "r_2", function(err, res){
+writeConcern.grant("user_1", "ggf");
+writeConcern.allow("user_1", "ggf", function(err, res){
     assert.equal(res, true);
     counter++;
 });
 
 
-//assert.equal(counter, 5);
 
-/*
-var readConcern = acl.createConcern("read", persistence, function(zoneId,resourceId){
-    if(writeConcern.allow(zoneId,resourceId)){
-        return true;
-    }
-    return false;
+var readConcern = acl.createConcern("read", persistence, null, function(zoneId, resourceId, callback){
+    var allow = writeConcern.allow.async(zoneId,resourceId);
+    (function(allow){
+        callback(null, allow);
+    }).wait(allow);
 });
-*/
+
+
+readConcern.allow("root", "ggm", function(err, res){
+    assert.equal(res, true);
+    counter++;
+});
+
+
+readConcern.allow("user_1", "ggm", function(err, res){
+    assert.equal(res, false);
+    counter++;
+    writeConcern.grant("user_1", "ggm");
+    readConcern.allow("user_1", "ggm", function(err, res) {
+        assert.equal(res, true);
+        counter++;
+    });
+});
+
+
+setTimeout(function(){
+    assert.equal(counter, 9);
+    console.log("Success!");
+}, 1000);
